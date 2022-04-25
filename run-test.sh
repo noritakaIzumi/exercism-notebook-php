d2cec0fb7fa05066b2eb5ddacf15afd9b137201e
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-04-25 21:00:00 +0900

    JUnit XML を CLI で閲覧するツールを導入

--- run-test.sh
+++ run-test.sh
@@ -11,11 +11,14 @@ script_dir="$(
     cd -- "$(dirname "${0}")" >/dev/null 2>&1
     pwd -P
 )"
+solution_dir="${script_dir}/${exercise_slug}"
 result_dir="${script_dir}/output/${exercise_slug}"
 
 mkdir -p "${result_dir}"
 
 MSYS_NO_PATHCONV=1 docker run --rm \
-    --mount type=bind,src="${script_dir}/${exercise_slug}",dst=/solution \
+    --mount type=bind,src="${solution_dir}",dst=/solution \
     --mount type=bind,src="${result_dir}",dst=/output \
     exercism/php-test-runner "${exercise_slug}" /solution /output
+
+npm run view-test-result "${result_dir}/results.xml"
