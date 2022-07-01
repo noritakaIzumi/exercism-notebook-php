8992e99c5efae9747c9c3b2079ac5dbe2e496daa
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-07-01 11:08:34 +0000

    add gitpod settings

---
+++ init-gitpod.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Install Exercism
+pushd /tmp
+wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-linux-64bit.tgz
+tar -xf exercism-linux-64bit.tgz
+cp -a ./exercism /usr/local/bin
+popd
+
+# Install packages
+npm Install
+
+# Build containers
+docker-compose up --remove-orphans
