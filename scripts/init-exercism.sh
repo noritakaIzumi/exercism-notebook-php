5b8860179d53fc6853fc35f2790f2697e0b27757
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-07-02 22:51:40 +0000

    fix directory names

--- scripts/init-exercism.sh
+++ scripts/init-exercism.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # install
-mkdir -p exercism
-pushd exercism
+mkdir -p tmp
+pushd tmp
 wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-linux-64bit.tgz
 tar -xf exercism-linux-64bit.tgz
 popd
-mv exercism/exercism .
-rm -r exercism
+mv tmp/exercism .
+rm -r tmp
 
 # configure
 ./exercism configure --token=${EXERCISM_TOKEN}
