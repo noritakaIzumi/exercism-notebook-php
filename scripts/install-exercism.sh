9905ce714f3a11d59b2b5445dbcee6750dac2e36
Noritaka IZUMI <noritaka.izumi@gmail.com>
2022-07-03 07:55:33 +0900

    fix gitpod init scripts

--- scripts/install-exercism.sh
+++ scripts/install-exercism.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# install
 mkdir -p tmp
 pushd tmp
 wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-linux-64bit.tgz
@@ -8,6 +7,3 @@ tar -xf exercism-linux-64bit.tgz
 popd
 mv tmp/exercism .
 rm -r tmp
-
-# configure
-./exercism configure --token=${EXERCISM_TOKEN}
